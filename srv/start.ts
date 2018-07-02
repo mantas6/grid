8aeea53a348ff641c1fdf9cd37886081f9a5b5da
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-07-02 19:23:36 +0300

    Signed By Mantas Pauliukonis

---
+++ srv/start.ts
@@ -0,0 +1,19 @@
+import { spawn, ChildProcess } from 'child_process';
+
+let handle: ChildProcess; 
+
+start();
+
+handle.on('exit', start);
+
+handle.stdout.on('data', buffer => message(buffer.toString()));
+handle.stderr.on('data', buffer => message(buffer.toString()));
+handle.on('error', err => message(err.message))
+
+function start() {
+    handle = spawn('ts-node', ['srv/main.ts']);
+}
+
+function message(msg: string) {
+    console.log(msg.replace("\n", ""));
+}
