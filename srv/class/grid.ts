b2d02761f9f4e9dfea81626d3b5dbb60c2aad534
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-07-04 11:37:35 +0300

    Signed By Mantas Pauliukonis

--- srv/class/grid.ts
+++ srv/class/grid.ts
@@ -21,8 +21,8 @@ export class Grid {
     }
 
     static generate(): Grid {
-        const sizeX = 64;
-        const sizeY = 64;
+        const sizeX = 32;
+        const sizeY = 32;
     
         const grid = new Grid(sizeX, sizeY);
     
