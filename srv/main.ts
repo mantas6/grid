f975b589daa500123440f23c4d98a6c3638ad81a
Mantas Pauliukonis <pauliukonis.mantas@gmail.com>
2018-07-05 14:41:17 +0300

    Signed By Mantas Pauliukonis

--- srv/main.ts
+++ srv/main.ts
@@ -134,7 +134,7 @@ io.on('connection', client => {
             filter(({ cell }) => (<Cell>cell).isOccupiable() || (<Cell>cell).isAbsorbable()),
             map(bundle => ({ ...bundle, distance: measureDistance(clientPlayer.cell.get(), bundle.cell) })),
             filter(({ distance }) => distance == 1),
-            filter(({ cell }) => clientPlayer.getStat('sta').affectByDiff(-1 * (<Cell>cell).size)),
+            filter(({ cell }) => clientPlayer.getStat('sta').affectByDiff(-1 * (<Cell>cell).size || 1)),
             tap(bundle => log.debug(`Position change request ${bundle.x} ${bundle.y}`)),
             tap(({ cell }) => ((<Cell>cell).isAbsorbable() && clientPlayer.absorbCell(cell)) || ((<Cell>cell).isOccupiable() && clientPlayer.assignCell(cell)))
         )
